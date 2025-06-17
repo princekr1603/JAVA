195b36057070d3529fbbdcc91a5cfc9b55adafc1
Prince <princekr1603@gmail.com>
2025-06-17 23:47:46 +0530

    add local scope

---
+++ Local.java
@@ -0,0 +1,10 @@
+public class Local {
+    {
+        int x=10;
+        System.out.println(x);
+    }
+    public static void main(String[] args) {
+        Local l=new Local();
+        System.out.println("hello local");
+    }
+}
