0af1c3e5ec680c491129ead8f7b287bab13c99d2
Prince <princekr1603@gmail.com>
2025-06-18 23:41:18 +0530

    default value

---
+++ Variables/global.java
@@ -0,0 +1,11 @@
+public class global {
+    int x;
+    double y;
+    String name;
+    public static void main(String[] args) {
+        global g=new global();
+        System.out.println("int: "+g.x);
+        System.out.println("double: "+g.y);
+        System.out.println("name: "+g.name);
+    }
+}
