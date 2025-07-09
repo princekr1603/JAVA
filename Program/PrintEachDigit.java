e9cb3c286ee43d0cad3ccabe2206f388500d30cd
Prince <princekr1603@gmail.com>
2025-07-09 22:36:40 +0530

    update

--- Program/PrintEachDigit.java
+++ Program/PrintEachDigit.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class PrintEachDigit {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("enter n: ");
+        System.out.print("enter number: ");
         int n=sc.nextInt();
         while(n!=0){
             int digit=n%10;
