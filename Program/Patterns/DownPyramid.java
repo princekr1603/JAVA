855c08ec8587536d577fed0c6f617465bfdf85d1
Prince <princekr1603@gmail.com>
2025-07-12 22:46:14 +0530

    update

--- Program/Patterns/DownPyramid.java
+++ Program/Patterns/DownPyramid.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-// Right triangle
-public class RightTriangle {
-    public static void main(String[] args) {
+
+public class DownPyramid {
+     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("enter n: ");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i>=j){
+            for(int j=1;j<=2*n-1;j++){
+                if(i<=j && i+j<=2*n){
                     System.out.print("* ");
                 }
                 else{
--- Program/Patterns/LeftDownTriangle.java
+++ Program/Patterns/LeftDownTriangle.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
-// Right triangle
-public class RightTriangle {
-    public static void main(String[] args) {
+
+public class LeftDownTriangle {
+     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         System.out.print("enter n: ");
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(i>=j){
+                if(i<=j){
                     System.out.print("* ");
                 }
                 else{
