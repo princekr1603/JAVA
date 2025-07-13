f349e6a9656002a23e6c2ad2e0f152db30c823b7
Prince <princekr1603@gmail.com>
2025-07-13 11:08:49 +0530

    update

--- Program/Patterns/RightRotatePyramid.java
+++ Program/Patterns/RightRotatePyramid.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class RightRotatePyramid {
      public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("enter n: ");
+        System.out.print("enter no: ");
         int n=sc.nextInt();
         for(int i=1;i<=2*n-1;i++){
             for(int j=1;j<=n;j++){
