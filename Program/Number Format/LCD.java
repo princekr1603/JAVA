71bec04e46b7006d2f6c9de61f0db131f3f2186e
Prince <princekr1603@gmail.com>
2025-07-13 22:07:58 +0530

    update

--- Program/Number Format/LCD.java
+++ Program/Number Format/LCD.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
 
 public class LCD {
-      public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print("enter n1: ");
-        int n1=sc.nextInt();
-        System.out.print("enter n2: ");
-        int n2=sc.nextInt();
-        int lcm=1;
-        for(int i=1;i<=n1 && i<=n2;i++){
-            if(n1%i==0 && n2%i==0){
-                lcm=i*i;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n1: ");
+        int n1 = sc.nextInt();
+        System.out.print("Enter n2: ");
+        int n2 = sc.nextInt();
+
+        int gcd = 1;
+        for (int i = 1; i <= n1 && i <= n2; i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                gcd = i;
             }
         }
-        System.out.println("GCD of "+n1+" and "+n2+" is: "+lcm);
+        // LCM using formula: LCM = (n1 * n2) / GCD
+        int lcm = (n1 * n2) / gcd;
+
+        System.out.println("GCD of " + n1 + " and " + n2 + " is: " + gcd);
+        System.out.println("LCM of " + n1 + " and " + n2 + " is: " + lcm);
+
         sc.close();
     }
 }
