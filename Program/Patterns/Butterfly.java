8ff82ef74b3c3d89c2d09a528e0879f44fe32000
Prince <princekr1603@gmail.com>
2025-07-14 14:19:36 +0530

    update

---
+++ Program/Patterns/Butterfly.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Butterfly {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=2*n-1;i++){
+            for(int j=1;j<=2*n-1;j++){
+                if((i>=j || i+j>=n*2) && (i+j<=2*n || i<=j)){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }    
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
