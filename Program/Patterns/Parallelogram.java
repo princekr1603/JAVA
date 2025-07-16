3b2f027aa767c4d79315e63d54d1facf49461ea8
Prince <princekr1603@gmail.com>
2025-07-16 22:44:44 +0530

    update

---
+++ Program/Patterns/Parallelogram.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Parallelogram {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=2*n-1;j++){
+                if(i<=j && j-i<=n-1)
+                    System.out.print("* ");
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ Program/Patterns/XinSquare.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class XinSquare {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n*2-1;i++){
+            for(int j=1;j<=n*2-1;j++){
+                if((i==1||j==1||i==n*2-1||j==n*2-1)||i==j||i+j==n*2){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }    
+            }
+            System.out.println();
+        }
+        sc.close();    
+    }
+}
