4da02ef2c7a67ae2116d072d2921bc6dd4bc974a
Prince <princekr1603@gmail.com>
2025-07-17 22:04:47 +0530

    update

--- Program/Patterns/Star.java
+++ Program/Patterns/Star.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Star {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        System.out.print("enter n: ");
+        System.out.print("enter n:- ");
         int n=sc.nextInt();
         for(int i=1;i<=n*2-1;i++){
             for(int j=1;j<=n*2-1;j++){
