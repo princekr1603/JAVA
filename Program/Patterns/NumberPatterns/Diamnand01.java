7565ff10f3b38c6ab5802040b51e40b35bb1e96f
Prince <princekr1603@gmail.com>
2025-07-19 09:50:07 +0530

    Diamnond01 patterns

---
+++ Program/Patterns/NumberPatterns/Diamnand01.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Diamnand01 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n*2-1;i++){
+            for(int j=1;j<=n*2-1;j++){
+                if(i+j>=n+1 && j-i<=n-1 && i-j<=n-1 && i+j<=3*n-1){
+                    System.out.print(i%2+" ");
+                } 
+                else{
+                    System.out.print("  ");
+                }  
+            }
+            System.out.println();
+        }
+        sc.close();    
+    }
+}
