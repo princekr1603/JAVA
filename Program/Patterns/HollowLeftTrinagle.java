58ebf27ba3e01e2904de35d32741e0535cb0563f
Prince <princekr1603@gmail.com>
2025-07-19 15:02:29 +0530

    update

--- Program/Patterns/HollowLeftTrinagle.java
+++ Program/Patterns/HollowLeftTrinagle.java
@@ -7,7 +7,7 @@ public class HollowLeftTrinagle {
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
-                if(i+j==n+1 || j==n || i==n){
+                if(i==j || j==n || i==1){
                     System.out.print("* ");
                 }
                 else{
